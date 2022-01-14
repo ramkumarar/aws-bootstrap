27d37b36c44f7326f9dc18582b6a5d6c7e88e081
ramkumarar <ramkumara.ar@gmail.com>
2022-01-14 18:57:07 +0530

    update for code pipeline

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -7,6 +7,11 @@ EC2_INSTANCE_TYPE=t2.micro
 AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap --query "Account" --output text`
 CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
 
+GH_ACCESS_TOKEN=$(cat ~/.github/aws-bootstrap-access-token)
+GH_OWNER=$(cat ~/.github/aws-bootstrap-owner)
+GH_REPO=$(cat ~/.github/aws-bootstrap-repo)
+GH_BRANCH=master
+
 # Deploys static resources
 echo -e "\n\n=========== Deploying setup.yml ==========="
 aws cloudformation deploy \
@@ -22,7 +27,12 @@ CodePipelineBucket=$CODEPIPELINE_BUCKET
 # Deploy the CloudFormation template
 echo -e "\n\n=========== Deploying main.yml ==========="
 
-aws cloudformation deploy --region $REGION --profile $CLI_PROFILE --stack-name $STACK_NAME --template-file ./main.yaml --capabilities CAPABILITY_NAMED_IAM --no-fail-on-empty-changeset --parameter-overrides EC2InstanceType=$EC2_INSTANCE_TYPE
+aws cloudformation deploy --region $REGION --profile $CLI_PROFILE --stack-name $STACK_NAME --template-file ./main.yaml --capabilities CAPABILITY_NAMED_IAM --no-fail-on-empty-changeset --parameter-overrides EC2InstanceType=$EC2_INSTANCE_TYPE \
+GitHubOwner=$GH_OWNER \
+GitHubRepo=$GH_REPO \
+GitHubBranch=$GH_BRANCH \
+GitHubPersonalAccessToken=$GH_ACCESS_TOKEN \
+CodePipelineBucket=$CODEPIPELINE_BUCKET
 
 # If the deploy succeeded, show the DNS name of the created instance
 if [ $? -eq 0 ]; then
