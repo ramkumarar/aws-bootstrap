575dfd4cb8345686d2f19032fa976f3332fd28b8
ramkumarar <ramkumara.ar@gmail.com>
2022-01-15 11:28:41 +0530

    Add a second instance

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -26,5 +26,5 @@ aws cloudformation deploy --region $REGION --profile $CLI_PROFILE --stack-name $
 if [ $? -eq 0 ]; then
 	aws cloudformation list-exports \
 		--profile awsbootstrap \
-		--query "Exports[?Name=='InstanceEndpoint'].Value" 
+		--query "Exports[?starts_with(Name,'InstanceEndpoint')].Value"
 fi
