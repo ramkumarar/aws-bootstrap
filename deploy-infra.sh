0a2dc17e80fbe6d448c84fedead05cd0c622fdfe
ramkumarar <ramkumara.ar@gmail.com>
2022-01-15 08:39:01 +0530

    pipeline fixes

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -13,26 +13,14 @@ GH_REPO=$(cat ~/.github/aws-bootstrap-repo)
 GH_BRANCH=master
 
 # Deploys static resources
-echo -e "\n\n=========== Deploying setup.yml ==========="
-aws cloudformation deploy \
---region $REGION \
---profile $CLI_PROFILE \
---stack-name $STACK_NAME-setup \
---template-file setup.yml \
---no-fail-on-empty-changeset \
---capabilities CAPABILITY_NAMED_IAM \
---parameter-overrides \
-CodePipelineBucket=$CODEPIPELINE_BUCKET
+aws cloudformation deploy --region $REGION --profile $CLI_PROFILE --stack-name $STACK_NAME-setup --template-file ./setup.yaml --capabilities CAPABILITY_NAMED_IAM --no-fail-on-empty-changeset --parameter-overrides  CodePipelineBucket=$CODEPIPELINE_BUCKET 
+
 
 # Deploy the CloudFormation template
 echo -e "\n\n=========== Deploying main.yml ==========="
 
-aws cloudformation deploy --region $REGION --profile $CLI_PROFILE --stack-name $STACK_NAME --template-file ./main.yaml --capabilities CAPABILITY_NAMED_IAM --no-fail-on-empty-changeset --parameter-overrides EC2InstanceType=$EC2_INSTANCE_TYPE \
-GitHubOwner=$GH_OWNER \
-GitHubRepo=$GH_REPO \
-GitHubBranch=$GH_BRANCH \
-GitHubPersonalAccessToken=$GH_ACCESS_TOKEN \
-CodePipelineBucket=$CODEPIPELINE_BUCKET
+aws cloudformation deploy --region $REGION --profile $CLI_PROFILE --stack-name $STACK_NAME --template-file ./main.yaml --capabilities CAPABILITY_NAMED_IAM --no-fail-on-empty-changeset --parameter-overrides EC2InstanceType=$EC2_INSTANCE_TYPE GitHubBranch=$GH_BRANCH CodePipelineBucket=$CODEPIPELINE_BUCKET GitHubOwner=$GH_OWNER GitHubRepo=$GH_REPO GitHubPersonalAccessToken=$GH_ACCESS_TOKEN
+
 
 # If the deploy succeeded, show the DNS name of the created instance
 if [ $? -eq 0 ]; then
