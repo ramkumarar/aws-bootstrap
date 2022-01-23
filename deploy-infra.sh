14e706a7417103917d3f013b5d6c2c79943bd9f4
ramkumarar <ramkumara.ar@gmail.com>
2022-01-23 13:38:19 +0530

    Enable https changes for the application

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -12,6 +12,12 @@ GH_ACCESS_TOKEN=$(cat ~/.github/aws-bootstrap-access-token)
 GH_OWNER=$(cat ~/.github/aws-bootstrap-owner)
 GH_REPO=$(cat ~/.github/aws-bootstrap-repo)
 GH_BRANCH=master
+DOMAIN=learncloud.ga
+CERT_DOMAIN=*.learncloud.ga
+
+CERT=`aws acm list-certificates --region $REGION --profile awsbootstrap --output text --query "CertificateSummaryList[?DomainName=='$CERT_DOMAIN'].CertificateArn | [0]"`
+
+echo $CERT
 
 # Deploys static resources
 aws cloudformation deploy --region $REGION --profile $CLI_PROFILE --stack-name $STACK_NAME-setup --template-file ./setup.yaml --capabilities CAPABILITY_NAMED_IAM --no-fail-on-empty-changeset --parameter-overrides  CodePipelineBucket=$CODEPIPELINE_BUCKET CloudFormationBucket=$CFN_BUCKET
@@ -40,7 +46,7 @@ fi
 # Deploy the CloudFormation template
 echo -e "\n\n=========== Deploying main.yml ==========="
 
-aws cloudformation deploy --region $REGION --profile $CLI_PROFILE --stack-name $STACK_NAME --template-file ./cfn_output/main.yaml  --capabilities CAPABILITY_NAMED_IAM --no-fail-on-empty-changeset --parameter-overrides EC2InstanceType=$EC2_INSTANCE_TYPE GitHubBranch=$GH_BRANCH CodePipelineBucket=$CODEPIPELINE_BUCKET GitHubOwner=$GH_OWNER GitHubRepo=$GH_REPO GitHubPersonalAccessToken=$GH_ACCESS_TOKEN
+aws cloudformation deploy --region $REGION --profile $CLI_PROFILE --stack-name $STACK_NAME --template-file ./cfn_output/main.yaml  --capabilities CAPABILITY_NAMED_IAM --no-fail-on-empty-changeset --parameter-overrides EC2InstanceType=$EC2_INSTANCE_TYPE Domain=$DOMAIN  Certificate=$CERT GitHubBranch=$GH_BRANCH CodePipelineBucket=$CODEPIPELINE_BUCKET GitHubOwner=$GH_OWNER GitHubRepo=$GH_REPO GitHubPersonalAccessToken=$GH_ACCESS_TOKEN
 
 
 # If the deploy succeeded, show the DNS name of the created instance
